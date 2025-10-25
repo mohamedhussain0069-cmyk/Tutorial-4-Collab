d870b091997834bf6bae7f04ee4ba182777f1645
mohamedhussain0069-cmyk <mohamedhussain0069@gmail.com>
2025-10-24 21:53:38 -0400

    Added sample file for tutorial

---
+++ Tutorial.py
@@ -0,0 +1,6 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> printf(Hello World);
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("Hi My name is Mohamed Hussain. I am doing Associated in IT and this is my first collabortation Tutorial")
+Hi My name is Mohamed Hussain. I am doing Associated in IT and this is my first collabortation Tutorial
